0b0aaf49e4880b284997f488cb27b8eaed18f206
Joe Pinsonault <joe.pinsonault@gmail.com>
2013-09-19 22:09:18 -0700

    Added function to compute neural network outputs

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -1,4 +1,5 @@
 import numpy
+from math import tanh
 
 
 class MultiNN:
@@ -17,8 +18,8 @@ class MultiNN:
         self.outputs = numpy.zeros((num_networks, num_outputs))
 
         # Buffers for calculations
-        self.hidden_sums = numpy.zeros((num_networks, num_hidden))
-        self.output_sums = numpy.zeros((num_networks, num_outputs))
+        self.hidden_sums = numpy.zeros((num_hidden))
+        self.hidden_outputs = numpy.zeros((num_hidden))
 
     def initialize_random_networks(self, limit):
         for index in range(self.num_weights):
@@ -42,11 +43,46 @@ class MultiNN:
         print(self.networks[nn_index])
 
     def compute_all_networks(self):
-        for network in self.networks:
-            self.compute_network(network)
+        for index, network in enumerate(self.networks):
+            self.compute_network(index)
 
-    def compute_network(self, network):
+    def compute_network(self, nn_index):
+        weights = self.networks[nn_index]
+        inputs = self.inputs[nn_index]
+        outputs = self.outputs[nn_index]
+
+        weights_index = 0
+
+        # Input-to-hidden weights
         for hidden_index in range(self.num_hidden):
             for input_index in range(self.num_inputs):
-                # self.hidden_sums[hidden_index] += self.inputs[input_index] * 
-                pass
+                self.hidden_sums[hidden_index] += inputs[input_index] * weights[weights_index]
+                weights_index += 1
+
+        # Hidden biases
+        for hidden_index in range(self.num_hidden):
+            self.hidden_sums[hidden_index] += weights[weights_index]
+            weights_index += 1
+
+        # Apply activation function
+        for hidden_index in range(self.num_hidden):
+            self.hidden_outputs[hidden_index] = self.hyperTan(self.hidden_sums[hidden_index])
+
+        # Hidden output weights
+        for output_index in range(self.num_outputs):
+            for hidden_index in range(self.num_hidden):
+                outputs[output_index] += self.hidden_outputs[hidden_index] * weights[weights_index]
+                weights_index += 1
+
+        # Output biases
+        for output_index in range(self.num_outputs):
+            outputs[output_index] += weights[weights_index]
+            weights_index += 1
+
+    def hyperTan(self, x):
+        if x < -20.0:
+            return -1.0
+        elif x > 20.0:
+            return 1.0
+        else:
+            return tanh(x)
