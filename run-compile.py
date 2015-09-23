f1c7a6e8367048bee1ad47c7f9a7e448c0913125
ben <benhgift@gmail.com>
2015-09-22 21:34:24 -0700

    changed "python" to "python3" in command

--- run-compile.py
+++ run-compile.py
@@ -3,8 +3,8 @@ from subprocess import call
 
 is_windows = len(platform.win32_ver()[0]) != 0
 
-compile_command = ["python", "setup.py", "build_ext", "--inplace"]
-run_command = ["python", "creature_simulation.py"]
+compile_command = ["python3", "setup.py", "build_ext", "--inplace"]
+run_command = ["python3", "creature_simulation.py"]
 
 if is_windows:
     compile_command.append("--compiler=mingw32")
