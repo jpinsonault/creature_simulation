1de063692e21b1295878f61a34546c5d92b0c595
Ben Gift <benhgift@gmail.com>
2016-04-19 13:53:00 -0700

    updated run-compile to be smart about your python version

--- run-compile.py
+++ run-compile.py
@@ -2,9 +2,10 @@ import platform
 from subprocess import call
 
 is_windows = len(platform.win32_ver()[0]) != 0
+python_name = 'python3' if platform.python_version() > '2.7' else 'python'
 
-compile_command = ["python3", "setup.py", "build_ext", "--inplace"]
-run_command = ["python3", "creature_simulation.py"]
+compile_command = [python_name, "setup.py", "build_ext", "--inplace"]
+run_command = [python_name, "creature_simulation.py"]
 
 if is_windows:
     compile_command.append("--compiler=mingw32")
